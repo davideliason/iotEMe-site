9884f21da254f8980f804542f77ac28bd2ad2b49
David Eliason <david@davethemaker.com>
2018-04-19 06:58:25 -0700

    add pubnub keys

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button, Grid, Row, Col } from 'react-bootstrap';
+import PubNubReact from 'pubnub-react';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+        this.state = {
+            messages : "",
+            gpsLocation: ""
+        };
+
+        this.pubnub = new PubNubReact({
+            publishKey: 'pub-c-c377ebaa-f828-40f5-8b64-9fef4ff4aeaa',
+            subscribeKey: 'sub-c-8b9161d0-391a-11e8-9da7-9e748936d455'
+        });
+  }
+
   render() {
     return (
       <div>
