2d54f49bb3c2b1bf9e07ce71ae0cb879d5792015
David Eliason <david@davethemaker.com>
2018-04-19 08:34:31 -0700

    push to vcs from root directory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AppContainer from './common/containers/appContainer.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
