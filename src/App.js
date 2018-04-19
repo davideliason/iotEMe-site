be6bb087ed73d97b8fc6db57cd73f56d32782e12
David Eliason <david@davethemaker.com>
2018-04-19 07:10:47 -0700

    add msg click handler to publish message, set state for that message, render new state data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
             gpsLocation: ""
         };
 
+        this.publishMessageToChannel = this.publishMessageToChannel.bind(this);
+
         this.pubnub = new PubNubReact({
             publishKey: 'pub-c-c377ebaa-f828-40f5-8b64-9fef4ff4aeaa',
             subscribeKey: 'sub-c-8b9161d0-391a-11e8-9da7-9e748936d455'
@@ -45,14 +47,29 @@ class App extends Component {
         });
     }
 
+      publishMessageToChannel(){
+         this.pubnub.publish({
+                message: Math.floor((Math.random() * 10) + 1) ,
+                channel: 'messageChannel'
+            });
+    }
+
   render() {
+    const messages = this.state.messages;
+
     return (
       <div>
         <Grid>
           <Row>
-              <Col xs={6} md={4}> <Button bsStyle="primary" >new msg</Button>  
+              <Col xs={6} md={4}> <Button bsStyle="primary" onClick={this.publishMessageToChannel}>new msg</Button>  
               </Col>
               <Col xs={6} md={4}> <Button bsStyle="success" >new gps</Button>
+              </Col>
+          </Row>
+          <Row>
+              <Col xs={6} md={4}>  {messages.message} 
+              </Col>
+              <Col xs={6} md={4}>
               </Col>
           </Row>
         </Grid>
