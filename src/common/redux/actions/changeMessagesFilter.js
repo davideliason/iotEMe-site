beb68aee77ae251d0378d401d1f67269e8b063f4
David Eliason <david@davethemaker.com>
2018-04-26 06:21:59 -0700

    refactor so both action creators reference actionTypes file instead of hard-coded

--- src/common/redux/actions/changeMessagesFilter.js
+++ src/common/redux/actions/changeMessagesFilter.js
@@ -1,6 +1,9 @@
+import {SET_MESSAGES_FILTER} from './actionTypes'
+
+
 export default function changeMessagesFilter(filter){
 	return {
-		type: 'SET_MESSAGES_FILTER',
+		type: SET_MESSAGES_FILTER,
 		filter
 	};
 }
