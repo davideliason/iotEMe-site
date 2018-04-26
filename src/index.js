3de8732a21e1e059040c7f69e3da0b51bf29da22
David Eliason <david@davethemaker.com>
2018-04-26 07:52:37 -0700

    successfully create store and display store state to console stdout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppContainer from './common/containers/appContainer.js';
+import {applyMiddleware,createStore} from 'redux';
+import {createLogger} from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from './common/redux/reducers/rootReducer';
 
+let logger = createLogger({
+	timestamp:true,
+	duration:true
+});
+
+const store = createStore(rootReducer, {}, applyMiddleware(thunk,logger));
+console.log(store.getState());
 ReactDOM.render(<AppContainer />, document.getElementById('root'));
 
