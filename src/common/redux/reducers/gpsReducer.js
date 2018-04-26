469c1cf5e88236416b92abaa399a3159e4ef7fd2
David Eliason <david@davethemaker.com>
2018-04-26 07:30:42 -0700

    add comments to code

--- src/common/redux/reducers/gpsReducer.js
+++ src/common/redux/reducers/gpsReducer.js
@@ -3,11 +3,13 @@ export default function gpsReducer(state={},action){
 		case 'gpsAdded': {
 			const newState = Object.assign({},state);
 			newState.gps = newState.gps || [];
+			// copy original state to not change it
 			newState.gps = newState.gps.slice();
+			// add new data payload to array and set state
 			newState.gps.push(action.gps);
 			return newState;
 		} 
-		
+
 		default:
 			return state;
 	}
--- src/common/redux/reducers/messagesReducer.js
+++ src/common/redux/reducers/messagesReducer.js
@@ -3,7 +3,9 @@ export default function messagesReducer(state={},action){
 		case 'messageAdded': {
 			const newState = Object.assign({},state);
 			newState.messages = newState.messages || [];
+			// copy original state to not change it
 			newState.messages = newState.messages.slice();
+			// add new data payload to array and set state
 			newState.messages.push(action.message);
 			return newState;
 		}
