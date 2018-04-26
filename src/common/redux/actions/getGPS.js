b79369ec2660de52230e5052099e059f37fd9042
David Eliason <david@davethemaker.com>
2018-04-26 13:56:39 -0700

    add action function for retrieving gps data from firebase

---
+++ src/common/redux/actions/getGPS.js
@@ -0,0 +1,34 @@
+import database from '../database.js';
+
+export default function getGPS(){
+	return dispatch => {
+		dispatch(getGPSRequestedAction());
+		return database.ref('/').once('value', snap => {
+			const gps = snap.val();
+			dispatch(getGPSFulfilledAction(gps))
+		})
+		.catch((error) => {
+			console.log(error);
+			dispatch(getGPSRejectedAction());
+		});
+	}
+}
+
+function getGPSRequestedAction() {
+  return {
+    type: 'GetGPSRequested'
+  };
+}
+
+function getGPSRejectedAction() {
+  return {
+    type: 'GetGPSRejected'
+  }
+}
+
+function getGPSFulfilledAction(books) {
+  return {
+    type: 'GetGPSFulfilled',
+    books
+  };
+}
