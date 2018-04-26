acb295302ed7fc1e6a0ff1f4ecb76c53fed54860
David Eliason <david@davethemaker.com>
2018-04-26 07:27:20 -0700

    add messages reducer taking the action and setting state by pushing the new action payload into the array

--- src/common/redux/reducers/messagesReducer.js
+++ src/common/redux/reducers/messagesReducer.js
@@ -1,6 +1,14 @@
 export default function messagesReducer(state={},action){
 	switch(action.type){
+		case 'messageAdded': {
+			const newState = Object.assign({},state);
+			newState.messages = newState.messages || [];
+			newState.messages = newState.messages.slice();
+			newState.messages.push(action.message);
+			return newState;
+		}
 		default:
 			return state;
 	}
 }
+
